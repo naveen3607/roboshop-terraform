45ec79d2349721c0a503940b552924fe29a70103
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-21 15:04:26 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -6,7 +6,8 @@ module "vpc" {
   default_vpc_id = var.default_vpc_id
   default_vpc_cidr = var.default_vpc_cidr
   default_vpc_route_table_id = var.default_vpc_route_table_id
-  tags = merge(var.tags, { env = var.env })
+  tags = var.tags
+  env = var.env
 }
 
 output "vpc" {
