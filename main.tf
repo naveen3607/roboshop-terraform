6273cced499f1cad4c303da540522cf2e02c42df
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-22 15:07:52 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -10,18 +10,18 @@ module "vpc" {
   env = var.env
 }
 
-module "alb" {
-  source            = "git::https://github.com/naveen3607/tf-module-alb.git"
-  for_each          = var.alb
-  internal = each.value["internal"]
-  lb_type = each.value["lb_type"]
-  sg_ingress_cidr = each.value["sg_ingress_cidr"]
-  vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-  subnets = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.id
-  tags = var.tags
-  env = var.env
-  sg_port = each.value["sg_port"]
-}
+#module "alb" {
+#  source            = "git::https://github.com/naveen3607/tf-module-alb.git"
+#  for_each          = var.alb
+#  internal = each.value["internal"]
+#  lb_type = each.value["lb_type"]
+#  sg_ingress_cidr = each.value["sg_ingress_cidr"]
+#  vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+#  subnets = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.id
+#  tags = var.tags
+#  env = var.env
+#  sg_port = each.value["sg_port"]
+#}
 
 
 output "vpc" {
