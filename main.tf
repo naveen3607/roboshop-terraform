7941fe7e61f320cb06049a8977c141b92e4d3d6e
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-24 14:47:23 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source            = "git::https://github.com/naveen3607/tf-module-vpc.git"
+  source            = "git::https://github.com/raghudevopsb74/tf-module-vpc.git"
+  #"git::https://github.com/naveen3607/tf-module-vpc.git"
   for_each          = var.vpc
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
@@ -11,13 +12,14 @@ module "vpc" {
 }
 
 module "alb" {
-  source            = "git::https://github.com/naveen3607/tf-module-alb.git"
+  source            = "git::https://github.com/raghudevopsb74/tf-module-alb.git"
+  #"git::https://github.com/naveen3607/tf-module-alb.git"
   for_each          = var.alb
   internal = each.value["internal"]
   lb_type = each.value["lb_type"]
   sg_ingress_cidr = each.value["sg_ingress_cidr"]
   vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-  subnets = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.id
+  subnets = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
   tags = var.tags
   env = var.env
   sg_port = each.value["sg_port"]
