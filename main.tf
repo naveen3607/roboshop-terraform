716052a49f8a9dcaf0a1d89a47102f0f1a26fe33
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-24 14:42:03 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "alb" {
   lb_type = each.value["lb_type"]
   sg_ingress_cidr = each.value["sg_ingress_cidr"]
   vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-  subnets = each.value["internal"] ? [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id] : data.aws_subnets.subnets.id
+  subnets = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.id
   tags = var.tags
   env = var.env
   sg_port = each.value["sg_port"]
