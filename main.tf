bef0d40fd0b7c948f802d5203ae4b5e65969ac12
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-25 03:21:02 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -32,6 +32,7 @@ module "docdb" {
   skip_final_snapshot     = each.value["skip_final_snapshot"]
   sg_ingress_cidr = local.app_subnet_cidr
   vpc_id = local.vpc_id
+  engine_version = each.value["engine_version"]
   tags = var.tags
   env = var.env
 }
