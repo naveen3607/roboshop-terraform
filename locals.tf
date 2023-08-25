ef7a2f87635df55b3dc790669a328b862a557baf
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-25 03:07:57 -0400

    Terraform Script

--- locals.tf
+++ locals.tf
@@ -2,4 +2,6 @@
    vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
    app_subnets = [ for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id ]
    db_subnets = [ for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "db", null), "subnet_ids", null) : v.id ]
+
+   app_subnet_cidr = [ for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.cidr_block ]
  }
--- main.tf
+++ main.tf
@@ -27,6 +27,11 @@ module "docdb" {
   source            = "git::https://github.com/naveen3607/tf-module-docdb.git"
   for_each          = var.docdb
   subnet_ids = local.db_subnets
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  sg_ingress_cidr = local.app_subnet_cidr
+  vpc_id = local.vpc_id
   tags = var.tags
   env = var.env
 }
